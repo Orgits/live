d3ee47ec296cfff6fa29df577c9292074c91ce3b
Anubhav Sharma <anubhav@Anubhavs-MacBook-Air.local>
2025-01-24 17:19:51 +0530

    ok

--- src/components/header/MainMenu.jsx
+++ src/components/header/MainMenu.jsx
@@ -11,30 +11,25 @@ const MainMenu = ({ toggleSubMenu, navbarPlacement }) => {
                 </li>
                 <li className="dropdown">
                 <Link href="/about-us" >About Us</Link>
-                    {/* <Link href="./about-us" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>About Us</Link> */}
-                    {/* <ul className="dropdown-menu">
-                        <li><Link href="./about-us">About Us</Link></li>
-                        <li><Link href="./">Team</Link></li>
-                        <li><Link href="./">FAQ</Link></li>
-                    </ul> */}
+                    
                 </li>
-                <li className="dropdown">
-                    <Link href="/services">Our Services</Link></li>
-                {/* <li className="dropdown">
-                <li><Link href="/services">Our Services</Link></li>
-                    {/* <Link href="/services" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Services</Link> */}
-                    {/* <ul className="dropdown-menu">
-                    <li><Link href="/services">Our Services</Link></li>
+                
+
+               <li className="dropdown">
+              
+                   <Link href="/services" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Services</Link> 
+                 <ul className="dropdown-menu">
+                   
                         <li><Link href="/services-details/1">Audit and Assurance</Link></li>
                         <li><Link href="/services-details/2">Report & Certification</Link></li>
                         <li><Link href="/services-details/3">Outsourcing Support Service </Link></li>
                         <li><Link href="/services-details/4">Governance, Risk & Compliance Advisory </Link></li>
-                        <li><Link href="#">Advisory & Assistance</Link></li>
+                        <li><Link href="services-details/30">Goods and Services Tax (GST)</Link></li>
                         <li><Link href="/services-details/5">FEMA Advisory</Link></li>
                         <li><Link href="/services-details/6">Corporate Law</Link></li>
-                        <li><Link href="#"> Formation & Registration Services</Link></li>
-                    </ul> */}
-                {/* </li> */}
+                        <li><Link href="/services-details/28"> Trademark Related Services</Link></li>
+                    </ul>
+              </li> 
                 <li className="dropdown">
                     <Link href="/client">Client</Link>
                 </li>
